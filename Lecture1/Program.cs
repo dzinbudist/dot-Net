fc8b9b9f362088d21a6f8f953168c3851fec462b
Andrius V <52530715+dzinbudist@users.noreply.github.com>
2019-09-21 12:51:51 +0100

    Updated

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Globalization;
 
-namespace P2_Simple_Console_Calculator
+namespace Drawing_Of_Figures
 {
     public class Program
     {
-        public static float aFirstValue, aSecondValue;
-        public static string aSymbol;
-
         public static void Main(string[] args)
         {
             Console.WriteLine("Piesiam eglute");
-            DrawFirTree("Iveskite eglutes eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+            SelectWhatToDraw("Pasirinkite ka noretumet nupiesti (iveskite numeri):");
+        }
+
+        static void SelectWhatToDraw(string aMessage)
+        {            
+            Console.WriteLine(aMessage);
+            Console.WriteLine("1 - Eglute");
+            Console.WriteLine("2 - Rombas");
+            Console.WriteLine("3 - Eglute kitu budu");
+
+            string aValue = Console.ReadLine();
+            //int aNo;
+
+            if (int.TryParse(aValue, out int aNo))
+            {
+                if (aNo == 1)
+                {
+                    DrawFirTree("Iveskite eglutes eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+                }
+                else if (aNo == 2)
+                {
+                    DrawDiamon("Iveskite rombo eiluciu kieki (gali buti tik sveikas skaicius, pvz 10)");
+                }
+                else if (aNo == 3)
+                {
+                    DrawFirTreeDiferent("Iveskite eglutes eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+                }
+                else
+                {
+                    SelectWhatToDraw("Rinkites is pateiktu numeriu");                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ivedete netinkama reiksme! Gasli buti tik sveikas skaicius.");
+                SelectWhatToDraw("Bandykite dar karta");
+            }
+
         }
 
         static void DrawFirTree(string aMessage)
@@ -56,7 +90,92 @@ namespace P2_Simple_Console_Calculator
                 }
 
                 Console.WriteLine("");
-                DrawFirTree("Nupieskite dar viena eglute - iveskite eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+                SelectWhatToDraw("Nupieskite dar ka nors");
+            }
+            else
+            {
+                Console.WriteLine("Netinkama reiksme!");
+                DrawFirTree("Iveskite eglutes eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+            }
+        }
+
+        static void DrawFirTreeDiferent(string aMessage)
+        {
+            int aLines;
+            Console.WriteLine(aMessage);
+            string aString = Console.ReadLine();
+
+            if (int.TryParse(aString, out aLines))
+            {
+                int aStars = 1, aStem = aLines / 3;
+                int aTotalSymbols = 1 + (aLines - 1) * 2;
+
+                //Console.WriteLine(new string('*', 1));
+
+                for (int i = 0; i < aLines; i++)
+                {
+                    Console.WriteLine((new string(' ', (aTotalSymbols - aStars) / 2)) + (new string('*', aStars))); 
+                    aStars = aStars += 2;
+                }
+
+                Console.WriteLine("");
+                SelectWhatToDraw("Nupieskite dar ka nors");
+            }
+            else
+            {
+                Console.WriteLine("Netinkama reiksme!");
+                DrawFirTree("Iveskite eglutes eiluciu kieki (gali buti tik sveikas skaicius, pvz 15)");
+            }
+        }
+
+        static void DrawDiamon(string aMessage)
+        {
+            int aLines;
+            Console.WriteLine(aMessage);
+            string aString = Console.ReadLine();
+
+            if (int.TryParse(aString, out aLines))
+            {
+                int aStars = 1;
+                int aFirstHalf = aLines / 2, aSecondtHalf = aLines / 2 - 1;
+                int aTotalSymbolsFirst = 1 + (aFirstHalf - 1) * 2;
+
+                for (int i = 0; i < aFirstHalf; i++)
+                {
+
+                    Console.WriteLine("");
+                    for (int i1 = 0; i1 < (aTotalSymbolsFirst - aStars) / 2; i1++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int i2 = 0; i2 < aStars; i2++)
+                    {
+                        Console.Write("*");
+                    }
+                    aStars = (aStars + 2);
+                }
+
+                aStars = (aStars - 4);
+
+                for (int i = 0; i < aSecondtHalf; i++)
+                {
+                    Console.WriteLine("");
+                    for (int i1 = 0; i1 < ((aTotalSymbolsFirst) - aStars) /2; i1++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int i2 = 0; i2 < aStars; i2++)
+                    {
+                        Console.Write("*");
+                    }
+                    aStars = (aStars - 2);
+                } 
+
+                Console.WriteLine("");
+                SelectWhatToDraw("Nupieskite dar ka nors");
+               
             }
             else
             {
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+
+namespace P2_Simple_Console_Calculator
+{
+    public class Program
+    {
+        public static float aFirstValue, aSecondValue;
+        public static string aSymbol;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Paprastas skaičiuotuvas. Programa gali sudėti, atimti, dauginti ir dalinti du vartotojo įvestus skaičius.");
+            CheckValue("Įveskite pirmą reikšmę", 1);
+        }
+
+        static void CheckValue(string aMessage, int aValueNo)
+        {
+            Console.WriteLine(aMessage);
+            float aConvertedFloat;
+            string aValue = Console.ReadLine();
+            aValue = aValue.Replace(',', '.');
+
+            if (String.IsNullOrEmpty(aValue))
+            {
+                Console.WriteLine("Reikšmė negali būti tuščia!");
+
+                if (aValueNo == 1)
+                {
+                    CheckValue("Įveskite pirmą reikšmę", 1);
+                }
+                else
+                {
+                    CheckValue("Įveskite antrą reikšmę", 2);
+                }
+            }
+            else
+            {
+                if (float.TryParse(aValue, out aConvertedFloat))
+                {
+                    if (aValueNo == 1)
+                    {
+                        aFirstValue = aConvertedFloat;
+                        GetOperator();
+                    }
+                    else
+                    {
+                        aSecondValue = aConvertedFloat;
+                        DoTheMath();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Netinkama reikšmė! Reikšmėje negali būti raidžių ir simbolių. Tinkamos reikšmės pvz: 5 arba 5.2");
+                    if (aValueNo == 1)
+                    {
+                        CheckValue("Įveskite pirmą reikšmę", 1);
+                    }
+                    else
+                    {
+                        CheckValue("Įveskite antrą reikšmę", 2);
+                    }
+                }
+            }
+        }
+
+        static void GetOperator()
+        {
+            Console.WriteLine("Įveskite matematinio veiksmo simbolį (+, -, *, /)");
+            aSymbol = Console.ReadLine();
+
+            if (aSymbol == "+" || aSymbol == "-" || aSymbol == "*" || aSymbol == "/")
+            {
+                CheckValue("Įveskite antrą reikšmę", 2);
+            }
+            else
+            {
+                Console.WriteLine("Neteisingas matematinio veiksmo simbolis!");
+                GetOperator();
+            }
+        }
+
+        static void DoTheMath()
+        {
+            if (aSymbol == "+")
+            {
+                Console.WriteLine(aFirstValue + " " + aSymbol + " " + aSecondValue + " = " + (aFirstValue + aSecondValue));
+                KeepWorking();
+            }
+            else if (aSymbol == "-")
+            {
+                Console.WriteLine(aFirstValue + " " + aSymbol + " " + aSecondValue + " = " + (aFirstValue - aSecondValue));
+                KeepWorking();
+            }
+            else if (aSymbol == "*")
+            {
+                Console.WriteLine(aFirstValue + " " + aSymbol + " " + aSecondValue + " = " + (aFirstValue * aSecondValue));
+                KeepWorking();
+            }
+            else if (aSymbol == "/")
+            {
+                if (aSecondValue != 0)
+                {
+                    Console.WriteLine(aFirstValue + " " + aSymbol + " " + aSecondValue + " = " + (aFirstValue / aSecondValue));
+                    KeepWorking();
+                }
+                else
+                {
+                    Console.WriteLine("Dalyba iš nulio negalima!");
+                    KeepWorking();
+                }
+            }
+        }
+
+        static void KeepWorking()
+        {
+            Console.WriteLine("Ar norite tęsti darbą?");
+            Console.WriteLine("Y/N");
+            string aValue = Console.ReadLine();
+
+            if (aValue.ToUpper() == "Y")
+            {
+                CheckValue("Įveskite pirmą reikšmę", 1);
+            }
+            else if (aValue.ToUpper() == "N")
+            {
+                Console.WriteLine("Viso geriausio");
+            }
+            else
+            {
+                KeepWorking();
+            }
+        }
+    }
+}
